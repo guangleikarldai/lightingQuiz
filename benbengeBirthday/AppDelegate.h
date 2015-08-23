97252480f96f6b70692a021bff5ffcb3109dfaf7
Guanglei Dai <guanglei.dai@fansentertainment.com>
2015-08-22 21:50:40 -0400

    Initial Commit for benbenge project

---
+++ benbengeBirthday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  benbengeBirthday
+//
+//  Created by Guanglei Dai on 2015-08-17.
+//  Copyright (c) 2015 bigbright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ benbengeBirthday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  benbengeBirthday
+//
+//  Created by Guanglei Dai on 2015-08-17.
+//  Copyright (c) 2015 bigbright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
