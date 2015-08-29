1815020b2dbb459de9044100a91975230916ac0e
Guanglei Dai <guanglei.dai@fansentertainment.com>
2015-08-28 21:52:02 -0400

    Add Navigation Controller

---
+++ benbengeBirthday/QuizTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizTableViewController.h
+//  benbengeBirthday
+//
+//  Created by Guanglei Dai on 2015-08-28.
+//  Copyright (c) 2015 bigbright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizTableViewController : UITableViewController
+
+@end
